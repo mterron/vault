49845889a41ed8d57d3125e63746e5538d267086
Miguel Terron <miguel.a.terron@gmail.com>
2016-07-06 18:12:58 +1200

    Dynamic Vault path

--- bin/start_vault.sh
+++ bin/start_vault.sh
@@ -24,13 +24,15 @@ done
 # "vault" as per https://www.vaultproject.io/docs/config/index.html#service
 VAULT_SERVICE_NAME=$(awk -F\" '/service =/{print $2}' /etc/vault/config.hcl | tr -d " /\"")
 
+VAULT_PATH=$(awk -F= '/path =/{print $2}' /etc/vault/config.hcl  | tr -d " /\"")
+
 # If VAULT_CONSUL_TOKEN environment variable is not set and there's no token on 
 # the Vault configuration file, create an ACL in Consul with access to Vault's 
 # "path" on the K/V store and the "vault" service key and acquire a token 
 # associated with that ACL
 if [ -z "${VAULT_CONSUL_TOKEN:-$(awk -F\" '/token/{print $2}' /etc/vault/config.hcl)}" ]; then
 	log 'Acquiring Consul token'
-	VAULT_CONSUL_TOKEN=$(consul-cli --token="$(awk -F\" '/acl_master_token/{print $4}' /consul/config/consul.json)" --consul="$CONSUL_HTTP_ADDR" acl create --name="$HOSTNAME Vault Token" --rule="key:$(awk -F= '/path =/{print $2}' /etc/vault/config.hcl  | tr -d " /\""):write" --rule="service:${VAULT_SERVICE_NAME:-vault}:write")
+	VAULT_CONSUL_TOKEN=$(consul-cli --token="$(awk -F\" '/acl_master_token/{print $4}' /consul/config/consul.json)" --consul="$CONSUL_HTTP_ADDR" acl create --name="$HOSTNAME Vault Token" --rule="key:${VAULT_PATH:-vault}:write" --rule="service:${VAULT_SERVICE_NAME:-vault}:write")
 fi
 REPLACEMENT_CONSUL_TOKEN="s/#*token = .*/token = \"${VAULT_CONSUL_TOKEN}\"/"
 sed -i "$REPLACEMENT_CONSUL_TOKEN" /etc/vault/config.hcl
