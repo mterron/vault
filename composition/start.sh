2e8c67e6d06dffa0032c7976e595d0c1753b56c5
mterron <miguel.a.terron@gmail.com>
2017-07-20 01:22:11 +1200

    Enhanced composition boot script

--- composition/start.sh
+++ composition/start.sh
@@ -2,11 +2,14 @@
 
 # check for prereqs
 command -v docker >/dev/null 2>&1 || { printf "%s\n" "Docker is required, but does not appear to be installed."; exit; }
+command -v jq >/dev/null 2>&1 || { printf "%s\n" "jq is required, but does not appear to be installed."; exit; }
+command -v vault >/dev/null 2>&1 || { printf "%s\n" "vault is required to config your cluster, but does not appear to be installed."; }
+test -e _env || { printf "%s\n" "_env file not found"; exit; }
 
 # default values which can be overriden by -f or -p flags
 export COMPOSE_FILE=
 export COMPOSE_PROJECT_NAME=demo
-export $(cat _env | grep CONSUL_CLUSTER_SIZE)
+export $(grep CONSUL_CLUSTER_SIZE _env)
 
 while getopts "f:p:" optchar; do
 	case "${optchar}" in
@@ -19,58 +22,65 @@ shift $(( OPTIND - 1 ))
 # give the docker remote api more time before timeout
 export COMPOSE_HTTP_TIMEOUT=300
 
-printf "%s\n" 'Starting a Consul service'
-printf "%s\n" '>Pulling the most recent images'
-#docker-compose pull
-printf "%s\n" '>Starting initial container'
+printf "%s\n\n" "Starting a ${COMPOSE_PROJECT_NAME} Vault cluster ▼ "
+printf "* Pulling the most recent images\n"
+docker-compose pull
+printf "* Starting initial container\n"
 docker-compose up -d --remove-orphans --force-recreate
 
 CONSUL_BOOTSTRAP_HOST="${COMPOSE_PROJECT_NAME}_vault_1"
-printf "%s\n" "CONSUL_BOOTSTRAP_HOST is $CONSUL_BOOTSTRAP_HOST"
 
 # Default for production
 BOOTSTRAP_UI_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$CONSUL_BOOTSTRAP_HOST")
-printf "UI: $BOOTSTRAP_UI_IP\n"
 
-export CONSUL_BOOTSTRAP_HOST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$CONSUL_BOOTSTRAP_HOST")
+export CONSUL_BOOTSTRAP_HOST="$BOOTSTRAP_UI_IP"
 
 # Wait for the bootstrap instance
-printf '>Waiting for the bootstrap instance...'
+printf ' >Waiting for the bootstrap instance...'
+sleep 5
 TIMER=0
-until curl -fs --connect-timeout 1 https://"$BOOTSTRAP_UI_IP":"${BOOTSTRAP_UI_PORT-8501}"/ui &>/dev/null
+until docker-compose -p "$COMPOSE_PROJECT_NAME" exec vault sh -c 'test -e /data/node-id'
 do
     if [ $TIMER -eq 20 ]; then
         break
     fi
     printf '.'
-    sleep 1
-    TIMER=$(( TIMER + 1))
+    sleep 5
+    TIMER=$(( TIMER + 5))
 done
+printf "\e[0;32m✔︎ \e[0m\n"
 
 printf "%s\n" 'The bootstrap instance is now running'
-printf "%s\n" "Dashboard: https://${BOOTSTRAP_UI_IP}:${BOOTSTRAP_UI_PORT}/ui/"
+printf "%s\n" "Dashboard: https://${BOOTSTRAP_UI_IP}:${BOOTSTRAP_UI_PORT:-8501}/ui/"
 # Open browser pointing to the Consul UI
-command -v open >/dev/null 2>&1 && open https://"$BOOTSTRAP_UI_IP":8501/ui/
+command -v open >/dev/null 2>&1 && open "https://$BOOTSTRAP_UI_IP:${BOOTSTRAP_UI_PORT:-8501}/ui/"
 
 # Scale up the cluster
 printf "%s\n" "Scaling the Consul raft to ${CONSUL_CLUSTER_SIZE} nodes"
 docker-compose -p "$COMPOSE_PROJECT_NAME" scale vault=$CONSUL_CLUSTER_SIZE
 
-printf '>Waiting for Consul cluster quorum acquisition and stabilisation'
-until docker-compose -p "$COMPOSE_PROJECT_NAME" exec vault /bin/ash -c 'consul info | grep leader_addr | grep "\d"'; do
+printf ' >Waiting for Consul cluster quorum acquisition and stabilisation...'
+until docker-compose -p "$COMPOSE_PROJECT_NAME" exec vault consul-cli status leader | jq -ce 'if . != "" then true else false end' >/dev/null
+do
 	printf '.'
-	sleep .2
+	sleep 5
 done
+printf "\e[0;32m✔︎ \e[0m\n"
 
-printf "%s\n" 'Initialising Vault'
-docker-compose -p "$COMPOSE_PROJECT_NAME" exec vault /bin/ash -c 'VAULT_ADDR="https://${HOSTNAME}.node.consul:8200" vault init -key-shares=1 -key-threshold=1'
+printf ">>> Cluster is running <<<\n\n"
+docker-compose -p "$COMPOSE_PROJECT_NAME" exec vault sh -c 'VAULT_ADDR="https://${HOSTNAME}.node.consul:8200" vault init -check'
+printf "Initialising Vault\n"
+docker-compose -p "$COMPOSE_PROJECT_NAME" exec vault sh -c 'VAULT_ADDR="https://${HOSTNAME}.node.consul:8200" vault init -key-shares=1 -key-threshold=1'
 for ((i=1; i <= CONSUL_CLUSTER_SIZE ; i++)); do
+	printf "%s\n" "Unsealing ${COMPOSE_PROJECT_NAME}_vault_$i"
 	docker-compose -p "$COMPOSE_PROJECT_NAME" exec --index="$i" vault unseal_vault.sh
 done
 export VAULT_ADDR="https://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' demo_vault_1):8200"
 export VAULT_SKIP_VERIFY=1
 export VAULT_TLS_SERVER_NAME=active.vault.service.consul
-export VAULT_CACERT=../tls/vault.service.consul.pem
+export VAULT_CAPATH=~/.vault/cert/
+export VAULT_CLIENT_CERT=~/.vault/cert/client_certificate.crt
+export VAULT_CLIENT_KEY=~/.vault/cert/client_certificate.key
 printf "Login to your new Vault cluster\n"
 vault auth
 printf "Enable Vault audit to file\n"
