53a53db8ae31fcb54d829dc2b9a8ba4414005179
mterron <miguel.a.terron@gmail.com>
2018-04-27 22:17:58 +1200

    Use the new syntax for the unseal command

--- bin/unseal_vault.sh
+++ bin/unseal_vault.sh
@@ -1,3 +1,3 @@
 #!/bin/ash
-VAULT_ADDR="https://${HOSTNAME}.node.${CONSUL_DOMAIN:-consul}:8200" vault unseal
+VAULT_ADDR="https://${HOSTNAME}.node.${CONSUL_DOMAIN:-consul}:8200" su-exec vault: vault operator unseal
 
