b0bb27b209e95a66ec1a563a7158cc4cf4e476f6
mterron <miguel.a.terron@gmail.com>
2018-04-27 22:17:00 +1200

    Remove dependency on consul-cli and replace it with curl calls

--- bin/start_vault.sh
+++ bin/start_vault.sh
@@ -1,9 +1,9 @@
 #!/bin/ash
 log() {
-	printf "%s\n" "$@"|awk '{print strftime("%FT%T%z",systime()),"[INFO] start_vault.sh:",$0}'
+	printf "start_vault.sh %s\n" "$@"
 }
 loge() {
-	printf "%s\n" "$@"|awk '{print strftime("%FT%T%z",systime()),"[ERROR] start_vault.sh:",$0}' >&2
+	printf "start_vault.sh [ERR] %s\n" "$@" >&2
 }
 
 # Add the Consul CA to the trusted list
@@ -13,23 +13,31 @@ if [ ! -e /etc/ssl/certs/ca-consul.done ]; then
 fi
 
 # Acquire Consul master token
-export CONSUL_HTTP_TOKEN="${CONSUL_ACL_MASTER_TOKEN:-$(jq -cr '.acl_master_token' /etc/consul/consul.json)}"
-
-# Wait for Consul to be available
-log 'Waiting for Consul instance...'
-until (consul-cli status leader | jq -e 'if . == "" then false else true end' >/dev/null); do
-	sleep 5s
+log "Waiting for Consul token"
+until [ -e /tmp/CT ]; do
+	sleep 2
 done
-log 'Consul is ready!'
+log "Consul token found!"
+export CONSUL_HTTP_TOKEN=$(cat /tmp/CT)
+rm -rf /tmp/CT
+
+set -e
 
 # Allow service & node discovery without a token
-consul-cli acl update --rule="node::read" --rule="service::read" anonymous
+log "Setting anonymous ACL for service discovery"
+su-exec consul curl -sS --unix-socket /data/consul.http.sock --header "X-Consul-Token: $CONSUL_HTTP_TOKEN" --data '{"ID": "anonymous",  "Type": "client",  "Rules": "node \"\" { policy = \"read\" } service \"\" { policy = \"read\" }"}' -XPUT http://consul/v1/acl/update
 
 # Get Vault service name from the environment or config file. If both are empty
 # it will default to "vault" as per
 # https://www.vaultproject.io/docs/config/index.html#service
-if [ -z "${VAULT_SERVICE_NAME:-$(jq -cr '.storage.consul.service' /etc/vault/config.json)}" ]; then
-	VAULT_SERVICE_NAME=vault
+if [ -z "$VAULT_SERVICE_NAME" ]; then
+	if [ "$(jq -cr '.storage.consul.service' /etc/vault/config.json)" != 'null' ]; then
+		VAULT_SERVICE_NAME="$(jq -cr '.storage.consul.service' /etc/vault/config.json)}"
+	else
+		VAULT_SERVICE_NAME=vault
+	fi
+else
+	VAULT_SERVICE_NAME="$VAULT_SERVICE_NAME"
 fi
 export VAULT_SERVICE_NAME
 
@@ -37,8 +45,8 @@ export VAULT_SERVICE_NAME
 export VAULT_PATH=$(jq -cr '.storage.consul.path' /etc/vault/config.json)
 
 # Remove old Vault service registrations
-if [ "${SERVICEID:-$(consul-cli agent services | jq -cr '.[].ID|select(. == "consul"|not)|select(.|contains("." + env.HOSTNAME + ".")|not)')}" ]; then
-	consul-cli service deregister "$SERVICEID"
+if [ "${SERVICEID:-$(su-exec consul curl -s --unix-socket /data/consul.http.sock http://consul/v1/agent/services | jq -cr '.[].ID|select(. == "consul"|not)|select(.|contains(env.HOSTNAME)|not)')}" ]; then
+	su-exec consul curl -sS --unix-socket /data/consul.http.sock --header "X-Consul-Token: $CONSUL_HTTP_TOKEN" -XPUT http://consul/v1/agent/service/deregister/"$SERVICEID"
 fi
 
 # If VAULT_CONSUL_TOKEN environment variable is not set and there's no token on
@@ -46,15 +54,19 @@ fi
 # "path" on the K/V store and the "vault" service key and acquire a token
 # associated with that ACL. Else use the environment variable if it exists or
 # the existing token (from the config file)
-if [ -z "${VAULT_CONSUL_TOKEN:-$(jq -cr '.storage.consul.token' /etc/vault/config.json)}" ]; then
-	log 'Acquiring a Consul token for Vault'
-	export VAULT_CONSUL_TOKEN=$(consul-cli acl create --name="$HOSTNAME Vault Token" --rule="key:${VAULT_PATH:-vault}:write" --rule="service:${VAULT_SERVICE_NAME:-vault}:write" --rule="node::write" --rule="agent::write" --rule="session::write" --rule="service::read")
-elif [ -z "$VAULT_CONSUL_TOKEN" ]; then
-	export VAULT_CONSUL_TOKEN=$(jq -cr '.storage.consul.token' /etc/vault/config.json)
+if [ -z "$VAULT_CONSUL_TOKEN" ]; then
+	if [ "$(jq -cr '.storage.consul.token' /etc/vault/config.json)" == 'null' ]; then
+#		log 'Acquiring a Consul token for Vault'
+#		export VAULT_CONSUL_TOKEN=$(su-exec consul curl -sS --unix-socket /data/consul.http.sock --header "X-Consul-Token: $CONSUL_HTTP_TOKEN" --data @/etc/consul/vault.policy -XPUT http://consul/v1/acl/create | jq -cre '.Token')
+		export VAULT_CONSUL_TOKEN=$CONSUL_HTTP_TOKEN
+	else
+		export VAULT_CONSUL_TOKEN=$(jq -cr '.storage.consul.token' /etc/vault/config.json)
+	fi
 fi
 
+
 # Set Consul token & Datacenter in the config file
-su -s /bin/sh vault -c "{ rm /etc/vault/config.json; jq '.storage.consul.service = env.VAULT_SERVICE_NAME | .storage.consul.token = env.VAULT_CONSUL_TOKEN | .storage.consul.datacenter = env.CONSUL_DC_NAME' > /etc/vault/config.json; } < /etc/vault/config.json"
+su-exec vault sh -c "{ rm /etc/vault/config.json; jq '.storage.consul.service = env.VAULT_SERVICE_NAME | .storage.consul.token = env.VAULT_CONSUL_TOKEN | .storage.consul.datacenter = env.CONSUL_DC_NAME' > /etc/vault/config.json; } < /etc/vault/config.json"
 
 
 # Fix privileges
@@ -68,7 +80,7 @@ fi
 
 # Vault redirect address
 export VAULT_REDIRECT_ADDR="https://${HOSTNAME}.node.${CONSUL_DOMAIN:-consul}:${VAULT_PORT:-8200}"
-export VAULT_ADDR="https://active.${VAULT_SERVICE_NAME:-vault}.service.${CONSUL_DOMAIN:-consul}:${VAULT_PORT:-8200}"
+export VAULT_ADDR="https://active.${VAULT_SERVICE_NAME}.service.${CONSUL_DOMAIN:-consul}:${VAULT_PORT:-8200}"
 
 # Unset local variables
 unset VAULT_PATH
@@ -76,5 +88,4 @@ unset VAULT_CONSUL_TOKEN
 unset VAULT_SERVICE_NAME
 unset CONSUL_HTTP_TOKEN
 
-log 'Starting Vault'
 exec su-exec vault:consul vault server -config=/etc/vault/config.json -log-level=warn
